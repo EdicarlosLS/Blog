1d4e88701aff64e551f25ed78ed8f9e036a5587d
EdicarlosLS <jels.23@hotmail.com>
2022-06-24 20:34:53 -0300

    Request renomeado e visualizacao de postagem

--- src/main/java/com/vulpeslab/blog/controllers/PostagemController.java
+++ src/main/java/com/vulpeslab/blog/controllers/PostagemController.java
@@ -5,6 +5,7 @@ import com.vulpeslab.blog.repositories.PostagemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,24 +15,32 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
-
-	@RequestMapping(value = "/novapostagem", method = RequestMethod.GET)
-	public String form() {
-		return "form_postagem";
-	}
-
-	@RequestMapping(value = "/novapostagem", method = RequestMethod.POST)
-        public String form(Postagem postagem) {
-		repository.save(postagem);
-         
-		return "form_postagem";
-	}
-
-	@RequestMapping("/postagens")
-	public ModelAndView listaPostagens() {
+	
+	@RequestMapping(value = "/postagens", method = RequestMethod.GET)
+	public ModelAndView lista() {
 		ModelAndView mv = new ModelAndView("index");
 		Iterable<Postagem> postagens = repository.findAll();
 		mv.addObject("postagens", postagens);
+		return mv;
+	}
+
+	@RequestMapping(value = "/postagens/nova", method = RequestMethod.GET)
+	public String nova() {
+		return "form_postagem";
+	}
+
+	@RequestMapping(value = "/postagens", method = RequestMethod.POST)
+        public String salvar(Postagem postagem) {
+		repository.save(postagem);         
+		return "redirect:/postagens/nova";
+	}
+
+	@RequestMapping(value = "/postagens/{id}")
+	public ModelAndView mostrar(@PathVariable long id){
+		ModelAndView mv = new ModelAndView("postagem");
+		Postagem p = repository.findById(id);
+		mv.addObject("postagem", p);
+		mv.addObject("id", id);
 		return mv;
 	}
 }
--- src/main/java/com/vulpeslab/blog/repositories/PostagemRepository.java
+++ src/main/java/com/vulpeslab/blog/repositories/PostagemRepository.java
@@ -4,7 +4,7 @@ import com.vulpeslab.blog.models.Postagem;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PostagemRepository extends CrudRepository<Postagem, String>{
+public interface PostagemRepository extends CrudRepository<Postagem, Long>{
 	Postagem findById(long id);
 }
 
